36772e30081ac1173a9b1dc3542eb207111eff67
Roman Butov <butoff@gmail.com>
2025-01-07 20:55:10 +0300

    docker & readme

---
+++ docker/build.sh
@@ -0,0 +1,35 @@
+IMAGE=aosp-course
+
+usage()
+{
+    echo 'Usage: build.sh [-f]' 1>&2
+    exit 1
+}
+
+if [ $# -gt 1 ]
+then
+    usage
+fi
+
+if [ $# -eq 1 ]
+then
+    if [ $1 = "-f" ]
+    then
+        force=true
+    else
+        usage
+    fi
+fi
+
+if [ -n "$(docker image ls -q $IMAGE)" ]
+then
+    if [ "$force" = true ]
+    then
+        docker rmi -f $IMAGE
+    else
+        echo "Image '$IMAGE' already exists. Launch with '-f' flag to rebuild." 1>&2
+        exit
+    fi
+fi
+
+docker build --build-arg userid=$(id -u) --build-arg groupid=$(id -g) --build-arg username=$(id -un) -t $IMAGE .
---
+++ docker/run.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v ~/aosp:/aosp aosp-course
